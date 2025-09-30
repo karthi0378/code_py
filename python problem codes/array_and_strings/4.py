6bef86d35712fde205821fd711769a7e19964b95
karthi0378 <karthiramakrishnan09@gmail.com>
2025-09-30 21:08:06 +0530

    adding

---
+++ python problem codes/array_and_strings/4.py
@@ -0,0 +1,23 @@
+#You have a long flowerbed in which some of the plots are planted, and some are not. However, 
+# flowers cannot be planted in adjacent plots.
+#Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not 
+# empty, and an integer n, return true if n new flowers can be planted in the flowerbed without 
+# violating the no-adjacent-flowers rule and false otherwise.
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = 0
+        length = len(flowerbed)
+        
+        for i in range(length):
+            if flowerbed[i] == 0:
+                empty_left = (i == 0) or (flowerbed[i - 1] == 0)
+                empty_right = (i == length - 1) or (flowerbed[i + 1] == 0)
+                
+                if empty_left and empty_right:
+                    flowerbed[i] = 1
+                    count += 1
+                    if count >= n:
+                        return True
+                    
+        return count >= n
